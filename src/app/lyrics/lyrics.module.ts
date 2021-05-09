abb99a0f073f5a1292bf950e2b64e00dfba1be4b
Phil Merrell <philmerrell@boisestate.edu>
2021-05-09 14:30:27 -0600

    chore: do not preload all modules

--- src/app/lyrics/lyrics.module.ts
+++ src/app/lyrics/lyrics.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LyricsPageRoutingModule } from './lyrics-routing.module';
 
 import { LyricsPage } from './lyrics.page';
+import { LyricsModalComponent } from './lyrics-modal/lyrics-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { LyricsPage } from './lyrics.page';
     IonicModule,
     LyricsPageRoutingModule
   ],
-  declarations: [LyricsPage]
+  declarations: [LyricsPage, LyricsModalComponent]
 })
 export class LyricsPageModule {}
